9dc305074f8823e0904739cf42629cf33a36e967
taylorjo02 <joshua.d.taylor122@gmail.com>
2024-12-23 10:52:13 -0500

    create node states for all tools and refactor player class

--- scenes/characters/player/idle_state.gd
+++ scenes/characters/player/idle_state.gd
@@ -3,7 +3,7 @@ extends NodeState
 @export var player: Player
 @export var animated_sprite_2d: AnimatedSprite2D
 
-var direction: Vector2
+var direction : Vector2
 
 func _on_process(_delta : float) -> void:
 	pass
@@ -11,18 +11,20 @@ func _on_process(_delta : float) -> void:
 
 func _on_physics_process(_delta : float) -> void:
 
+
 	direction = GameInputEvents.movement_input()
 
-	if player.player_direction == Vector2.UP:
-		animated_sprite_2d.play("idle_back")
-	elif player.player_direction == Vector2.RIGHT:
-		animated_sprite_2d.play("idle_right")
-	elif player.player_direction == Vector2.DOWN:
-		animated_sprite_2d.play("idle_front")
-	elif player.player_direction == Vector2.LEFT:
-		animated_sprite_2d.play("idle_left")
-	else:
-		animated_sprite_2d.play("idle_front")
+	if player != null:
+		if player.player_direction == Vector2.UP:
+			animated_sprite_2d.play("idle_back")
+		elif player.player_direction == Vector2.RIGHT:
+			animated_sprite_2d.play("idle_right")
+		elif player.player_direction == Vector2.DOWN:
+			animated_sprite_2d.play("idle_front")
+		elif player.player_direction == Vector2.LEFT:
+			animated_sprite_2d.play("idle_left")
+		else:
+			animated_sprite_2d.play("idle_front")
 
 
 func _on_next_transitions() -> void:
@@ -30,11 +32,22 @@ func _on_next_transitions() -> void:
 
 	if GameInputEvents.is_movement_input():
 		transition.emit("Walk")
+	
+	if player != null:
+
+		if player.current_tool == DataTypes.Tools.AxeWood && GameInputEvents.use_tool():
+			transition.emit("Chopping")
+
+		if player.current_tool == DataTypes.Tools.TillGround && GameInputEvents.use_tool():
+			transition.emit("Tilling")
+
+		if player.current_tool == DataTypes.Tools.WaterCrops && GameInputEvents.use_tool():
+			transition.emit("Watering")
 
 
 func _on_enter() -> void:
-	pass
+	player = get_parent().get_node("/root/TestSceneTilemap/Player")
 
 
 func _on_exit() -> void:
-	animated_sprite_2d.stop()
+	pass
--- scenes/characters/player/walk_state.gd
+++ scenes/characters/player/walk_state.gd
@@ -5,35 +5,47 @@ extends NodeState
 @export var speed: int = 50
 
 func _on_process(_delta : float) -> void:
-	pass
-
+    pass
 
 func _on_physics_process(_delta : float) -> void:
-	var direction: Vector2 = GameInputEvents.movement_input()
+    if player == null:
+        print("Player is null in _on_physics_process")
+        return
 
-	if direction == Vector2.UP:
-		animated_sprite_2d.play("walk_back")
-	elif direction == Vector2.RIGHT:
-		animated_sprite_2d.play("walk_right")
-	elif direction == Vector2.DOWN:
-		animated_sprite_2d.play("walk_front")
-	elif direction == Vector2.LEFT:
-		animated_sprite_2d.play("walk_left")
+    var direction: Vector2 = GameInputEvents.movement_input()
 
-	if direction != Vector2.ZERO:
-		player.player_direction = direction
+    if direction == Vector2.UP:
+        animated_sprite_2d.play("walk_back")
+    elif direction == Vector2.RIGHT:
+        animated_sprite_2d.play("walk_right")
+    elif direction == Vector2.DOWN:
+        animated_sprite_2d.play("walk_front")
+    elif direction == Vector2.LEFT:
+        animated_sprite_2d.play("walk_left")
 
-	player.velocity = direction * speed
-	player.move_and_slide()
+    if direction != Vector2.ZERO:
+        player.player_direction = direction
+
+    player.velocity = direction * speed
+    player.move_and_slide()
 
 func _on_next_transitions() -> void:
-	if !GameInputEvents.is_movement_input():
-		transition.emit("Idle")
+    if player == null:
+        print("Player is null in _on_next_transitions")
+        return
 
+    if !GameInputEvents.is_movement_input():
+        transition.emit("Idle")
 
 func _on_enter() -> void:
-	pass
-
+    print("Entering walk state")
+    var parent = get_parent()
+    print("Parent node: ", parent)
+    player = parent.get_parent().get_node("/root/TestSceneTilemap/Player")
+    if player == null:
+        print("Player is null in _on_enter")
+    else:
+        print("Player initialized in _on_enter")
 
 func _on_exit() -> void:
-	animated_sprite_2d.stop()
+    pass
