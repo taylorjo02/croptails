d14f6ce253985be1e6a7358d33ade01d0580bfc1
taylorjo02 <joshua.d.taylor122@gmail.com>
2024-12-26 14:01:22 -0500

    add log and collectable component

--- scenes/objects/tree/small_tree.gd
+++ scenes/objects/tree/small_tree.gd
@@ -3,6 +3,8 @@ extends Sprite2D
 @onready var hurt_component: HurtComponent = $HurtComponent
 @onready var damage_component: DamageComponent = $DamageComponent
 
+var log_scene = preload("res://scenes/objects/tree/log.tscn")
+
 func _ready() -> void:
   hurt_component.hurt.connect(on_hurt)
   damage_component.max_damage_reached.connect(on_max_damage_reached)
@@ -11,5 +13,11 @@ func on_hurt(hit_damage: int) -> void:
   damage_component.apply_damage(hit_damage)
 
 func on_max_damage_reached() -> void:
+  call_deferred("add_log_scene")
   print ("Tree Destroyed due to max damage reached")
   queue_free()
+
+func add_log_scene() -> void:
+  var log_instance = log_scene.instantiate() as Node2D
+  log_instance.global_position = global_position
+  get_parent().add_child(log_instance)
